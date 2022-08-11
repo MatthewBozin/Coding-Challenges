465ca9c27af96d89718228d72625ef0b5f6c2a52
Matthew Bozin <mbozin94@gmail.com>
2022-08-10 23:36:03 -0700

    Coding challenge for 08-10-2022

---
+++ 08-10-2022/app.js
@@ -0,0 +1,7 @@
+//Simple, remove the spaces from the string, then return the resultant string.
+
+function noSpace(x){
+  return x.replaceAll(' ', '')
+}
+
+console.log(noSpace('8 j 8   mBliB8g  imjB8B8  jl  B'));
