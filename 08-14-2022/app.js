828927a71e11af34bb4a672d00ec0fb139a4dafa
Matthew Bozin <mbozin94@gmail.com>
2022-08-14 22:22:49 -0700

    Coding challenge for 08-14-2022

---
+++ 08-14-2022/app.js
@@ -0,0 +1,2 @@
+//Write a function that takes an array of numbers and returns the sum of the numbers. The numbers can be negative or non-integer. If the array does not contain any numbers then you should return 0.
+const sum = numbers => numbers.reduce((acc, curr) => acc+curr,0);
