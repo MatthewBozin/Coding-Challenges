763f6ebc050123048208762e81c81f6de8fee9c1
Matthew Bozin <mbozin94@gmail.com>
2022-08-28 09:57:50 -0700

    Coding challenge for 08-28-2022

---
+++ 08-28-2022/app.js
@@ -0,0 +1,4 @@
+// Write a function that takes in a string of one or more words, and returns the same string, but with all five or more letter words reversed (Just like the name of this Kata). 
+// Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present.
+
+const spinWords = string => string.split(" ").map(word => word.length > 4 ? word.split("").reverse().join("") : word).join(" ");
