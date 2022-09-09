8189ae20cb5793b04fea682fea9334522df9badd
Matthew Bozin <mbozin94@gmail.com>
2022-09-09 11:32:00 -0700

    Coding challenge for 09-09-2022

---
+++ 09-09-2022/app.js
@@ -0,0 +1,3 @@
+//In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
+
+const filter_list = l => l.filter(el => typeof el === "number");
