d9f8dc6042e794d257fb86a5d63df3ebe8e1a7e5
Matthew Bozin <mbozin94@gmail.com>
2022-10-12 15:08:01 -0700

    Updated coding challenge for 10-12-2022

--- 10-12-2022/app.js
+++ 10-12-2022/app.js
@@ -5,13 +5,14 @@
 
 const fizzBuzz = (num) => {
     for (i = 1; i <= num; i++) {
-        console.log(i)
         if (i % 3 === 0 && i % 5 === 0) {
             console.log('Fizz Buzz')
         }else if (i % 3 === 0) {
             console.log('Fizz')
         } else if (i % 5 === 0) {
             console.log('Buzz')
+        } else {
+            console.log(i)
         }
     }
 }
