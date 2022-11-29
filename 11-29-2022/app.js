ec9d4732eddb714c664339bc2a727947865f5c94
Matthew Bozin <mbozin94@gmail.com>
2022-11-29 12:08:57 -0800

    Coding challenge for 11-29-2022

---
+++ 11-29-2022/app.js
@@ -0,0 +1,19 @@
+// You will be given an array of strings and your task is to remove all consecutive duplicate letters from each string in the array.
+
+// For example:
+// dup(["abracadabra","allottee","assessee"]) = ["abracadabra","alote","asese"].
+// dup(["kelless","keenness"]) = ["keles","kenes"].
+// Strings will be lowercase only, no spaces 
+
+const dup = arr => arr.map(str => {
+    let last = ''
+    return [...str].filter(char => {
+        let newLast = last
+        last = char
+        return char !== newLast
+    }).join('')
+})
+
+console.log(dup(["ccooddddddewwwaaaaarrrrsssss","piccaninny","hubbubbubboo"]),['codewars','picaniny','hubububo'])
+console.log(dup(["abracadabra","allottee","assessee"]),['abracadabra','alote','asese'])
+console.log(dup(["kelless","keenness"]), ['keles','kenes'])
