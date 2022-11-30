a4f8848e4133ea8afde7375194dd52225ccbcdc2
Matthew Bozin <mbozin94@gmail.com>
2022-11-30 12:28:19 -0800

    Coding challenge for 11-30-2022

---
+++ 11-30-2022/app.js
@@ -0,0 +1,21 @@
+// Your job is to figure out the index of which vowel is missing from a given string:
+// A has an index of 0,
+// E has an index of 1,
+// I has an index of 2,
+// O has an index of 3,
+// U has an index of 4.
+// Notes: There is no need for string validation and every sentence given will contain all vowels but one. Also, you won't need to worry about capitals.
+
+// Examples
+// "John Doe hs seven red pples under his bsket"          =>  0  ; missing: "a"
+// "Bb Smith sent us six neatly arranged range bicycles"  =>  3  ; missing: "o"
+
+const absentVowel = str => {
+    const vowels = ['a','e','i','o','u']
+    for (i = 0; i < 5; i++) {
+        if (!str.toLowerCase().includes(vowels[i])) return i
+    }
+}
+
+console.log(absentVowel("John Doe hs seven red pples under his bsket"), 0);
+console.log(absentVowel("Bb Smith sent us six neatly arranged range bicycles"), 3);
